116200390773fa6045ce5f88e993c6a35a5fcd9c
Miraz Ali <mirazali1998@gmail.com>
2021-05-24 01:50:45 +0530

    update all

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import main from './styles/main.scss'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
